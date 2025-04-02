11a53e00d992a99c322ea283b7d68ccf10bb3da5
Hadi Gatz <albertsviridovski@gmail.com>
2025-04-02 09:00:26 +0300

    [FIX] fixed the server side, in order for it to be suited for receiving UDP packets

--- client.py
+++ client.py
@@ -1,11 +1,11 @@
 from scapy.all import *
 
-SERVER_IP = '192.168.68.105'
+SERVER_IP = '127.0.0.1'
 def send_message(message):
     for letter in message:
         port = ord(letter) + 1000
-        packet = Ether()/IP(dst=SERVER_IP)/UDP(dport=port)/Raw(load='.')
-        sendp(packet)
+        packet = Ether()/IP(dst=SERVER_IP)/UDP(dport=port)
+        send(packet)
 
 send_message("hello")
 
--- server.py
+++ server.py
@@ -2,13 +2,17 @@ import socket
 from threading import Thread
 
 def listening_to_port(port_number):
-    listener = socket.socket()
+    listener = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
     listener.bind(('', port_number))
-    listener.listen()
-    client, addr = listener.accept()
-    print(chr(port_number))
+    while True:
+        data, addr = listener.recvfrom(1024)  
+        print(chr(port_number - 1000), end='')
 
-port_listener = Thread(target=listening_to_port, args=(1032,))
+listeners = []
+space_listener = Thread(target=listening_to_port, args=(1032,))
+space_listener.start()
+listeners.append(space_listener)
 for i in range(1097, 1122):
     port_listener = Thread(target=listening_to_port, args=(i,))
     port_listener.start()
+    listeners.append(port_listener)
